da3d89a78ce45ae8b8a5efa1e830cdd8c51f1b90
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-05-30 09:50:45 +0600

    react router explored

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NoMatch from './Components/NoMatch/NoMatch';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route path="/home">
+          <Home/>
+        </Route>
+
+        <Route path="*">
+            <NoMatch />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/NoMatch/NoMatch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NoMatch() {
+    return (
+      <div>
+        <h3>
+          Error 404!
+        </h3>
+      </div>
+    );
+  }
+
+  export default NoMatch;  
